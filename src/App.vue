b8ffb1277c76e2a7327b3d31b87e4d25fcf00618
Artyom Yeghoyan <artyomyeghoyan@gmail.com>
2021-06-05 13:26:26 +0300

    feature: added styles and fonts

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,41 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import url("https://fonts.googleapis.com/css2?family=Kelly+Slab&family=Roboto&family=Spectral:wght@700&display=swap");
 
-#nav {
-  padding: 30px;
+* {
+  vertical-align: baseline;
+  font-weight: inherit;
+  font-family: inherit;
+  font-style: inherit;
+  font-size: 100%;
+  border: 0 none;
+  outline: 0;
+  padding: 0;
+  margin: 0;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "Spectral", serif;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+p {
+  font-family: "Roboto", sans-serif;
+}
+ul {
+  list-style: outside none none;
+}
+.link {
+  text-decoration: none;
+}
+.text-cursive {
+  font-family: "Kelly Slab", cursive;
 }
 </style>
